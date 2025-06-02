a870521e8c0fb4b9c14f9bac87ea977b2026bd37
Alex Varella <atitudehcal@gmail.com>
2025-06-02 06:47:39 -0300

    Ensure vite.config.js base is applied and clean build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite'; // Importa o plugin do Preact
+import preact from '@preact/preset-vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()], // Usa o plugin do Preact
-  base: '/simulador-appsheet/', // <-- ESSA LINHA É CRÍTICA!
-                                // Ela define a base URL para o GitHub Pages.
-                                // Deve ser o nome do seu repositório envolto em barras.
+  plugins: [preact()],
+  base: '/simulador-appsheet/',
 });
